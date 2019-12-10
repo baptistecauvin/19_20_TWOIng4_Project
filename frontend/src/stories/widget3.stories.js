62908a0f40a64930c6d31a95468e87b60835542a
baptistecauvin <baptiste.cauvin@edu.ece.fr>
2019-12-10 22:47:17 +0100

    test storybook ne fonctionne pas

---
+++ frontend/src/stories/widget3.stories.js
@@ -0,0 +1,8 @@
+/*import React from 'react';
+import { storiesOf } from '@storybook/react';
+import Widget3 from './Widget3/widget3';
+
+storiesOf('Widget3', module)
+
+	.add('default', () => <Widget3 />
+		);*/
